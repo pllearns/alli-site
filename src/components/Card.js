551f6528a49cea63083122ad95940d8e376185a5
pllearns <pllearns@gmail.com>
2018-11-07 19:53:25 -0800

    Update card component with code clean up

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,95 +16,78 @@ const WithWidth = toRenderProps(withWidth());
 
 const styles = {
   card: {
-    maxWidth: 345,
+    maxWidth: 450,
   },
   media: {
     height: 140,
   },
 };
 
+
+const services = [
+  {
+    id: 1,
+    title: 'events',
+    heading: 'Find events near you',
+    description: 'Alli can help find other developers like you through meetups.',
+    image: '/static/images/events_pic.png'
+  },
+  {
+    id: 2,
+    title: 'jobs',
+    heading: 'Find jobs',
+    description: 'Have a conversation with Alli so she can help you find a job.',
+    image: '/static/images/jobs_6.png'
+
+  },
+  {
+    id: 3,
+    title: 'mentors',
+    heading: 'Mentorship',
+    description: 'Alli can find opportunities for you to mentor a junior developer.',
+    image: '/static/images/mentor_1.png'
+  }
+]
+
 function MediaCard(props) {
   const { classes } = props;
+
   return (
     <WithWidth>
       {({ width }) => <div>
-        <Grid container spacing={40} justify='center'>
-          <Grid item xs={3}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image="/static/images/events_pic.png"
-                  title="events"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Find Events Near You
-          </Typography>
-                  <Typography component="p">
-                    Alli can help find other developers like you through meetups.
-          </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Learn More
+        {services.map((service) =>
+          <Grid container spacing={40}>
+            <Grid item xs={3}>
+              <Card key={service.id}
+                className={classes.card}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={service.image}
+                    title={service.title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {service.heading}
+                    </Typography>
+                    <Typography component="p">
+                      {service.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Learn More
         </Button>
-              </CardActions>
-            </Card>
+                </CardActions>
+              </Card>
+            </Grid>
           </Grid>
-          <Grid item xs={3}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image="/static/images/jobs_6.png"
-                  title="jobs"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Find Jobs
-          </Typography>
-                  <Typography component="p">
-                    Have a conversation with Alli so she can help you find a job.
-          </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Learn More
-        </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid item xs={3}>
-            <Card className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image="/static/images/mentor_1.png"
-                  title="mentors"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Mentorship
-          </Typography>
-                  <Typography component="p">
-                    Alli can find opportunities for you to mentor a junior developer.
-          </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Learn More
-        </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        </Grid>
-      </div>}
+        )}
+      </div>
+      }
     </WithWidth>
-  );
+  )
 }
 
 MediaCard.propTypes = {
